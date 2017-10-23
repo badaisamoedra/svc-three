779bc9072681fb972559a6981422787eb885bd7d
badai samoedra <badai.samoedra@outlook.com>
2017-10-23 18:15:28 +0700

    i love you mas andi

---
+++ tests/BasicTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class BasicTest extends TestCase
+{
+     public function testExample()
+     {
+          $this->json('POST', '/posts', ['title' => 'yance_kece', 'body' => 'payung rindu'])
+          ->seeJson([
+               'title' => 'yance_kece',
+               'body' => 'payung rindu'
+          ]);
+     }
+
+     public function testApplication()
+     {
+          $response = $this->call('GET', '/posts');
+          $this->assertEquals(200, $response->status());
+     }
+
+     public function testGanteng()
+     {
+          $response = $this->call('PUT', '/posts/8', ['title' => 'Taylor']);
+          $this->assertEquals(200, $response->status());
+     }
+
+}
